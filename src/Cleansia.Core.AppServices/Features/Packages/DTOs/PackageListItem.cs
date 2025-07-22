5d76b1a7fe31ffdec2823336f0e9aef32661715d
Michael Chaban <cmisa695@gmail.com>
2025-07-22 22:49:28 +0200

    feat(packages): Implement Package and PackageService classes with DTOs and integration tests

---
+++ src/Cleansia.Core.AppServices/Features/Packages/DTOs/PackageListItem.cs
@@ -0,0 +1,10 @@
+﻿using Cleansia.Core.Domain.Internalization;
+
+namespace Cleansia.Core.AppServices.Features.Packages.DTOs;
+
+public record PackageListItem(
+    string Id,
+    string Name,
+    string Description,
+    decimal Price,
+    Dictionary<string, Translation> Translations);
---
+++ src/Cleansia.Core.AppServices/Features/Packages/GetPackageOverview.cs
@@ -0,0 +1,20 @@
+﻿using Cleansia.Core.AppServices.Features.Packages.DTOs;
+using Cleansia.Core.AppServices.Mappers;
+using Cleansia.Core.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cleansia.Core.AppServices.Features.Packages;
+
+public class GetPackageOverview
+{
+    public record Request : IRequest<IEnumerable<PackageListItem>>;
+
+    public class Handler(IPackageRepository packageRepository) : IRequestHandler<Request, IEnumerable<PackageListItem>>
+    {
+        public async Task<IEnumerable<PackageListItem>> Handle(Request request, CancellationToken cancellationToken)
+        {
+            return await packageRepository.GetAll().Select(package => package.MapToDto()).ToListAsync(cancellationToken);
+        }
+    }
+}

71a6bd557fd5520a18bda891ee34c5ca05b25a0b
Michael Chaban <cmisa695@gmail.com>
2026-03-21 13:42:07 +0100

    feat: update deployment scripts and configurations for Partner API in DEV environment

---
+++ src/Cleansia.App/apps/cleansia-partner.app/src/environments/environment.staging.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  apiHost: 'api-cleansia-partner-dev.azurewebsites.net',
+  apiPort: '443',
+  apiProtocol: 'https',
+  apiBaseUrl: 'https://api-cleansia-partner-dev.azurewebsites.net',
+  blobStorageUrl: '',
+  googleClientId: '',
+  betaGateEnabled: false,
+  betaGateUrl: '',
+  isDevelopment: false,
+  sentryDsn: '',
+};

465b45028485f05c03c7867f25299ba03b50650b
Michael Chaban <cmisa695@gmail.com>
2026-03-25 22:34:40 +0100

    fix: add Application Insights Worker Service package and configure telemetry in functions

--- src/Cleansia.Web/Controllers/CodeController.cs
+++ src/Cleansia.Web/Controllers/CodeController.cs
@@ -2,6 +2,7 @@
 using Cleansia.Core.AppServices.Shared.DTOs.Enums;
 using Cleansia.Web.Abstractions;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cleansia.Web.Controllers;
@@ -11,6 +12,7 @@ namespace Cleansia.Web.Controllers;
 public class CodeController(IMediator mediator) : ApiController(mediator)
 {
     [HttpGet("GetOverview")]
+    [AllowAnonymous]
     [ProducesResponseType(typeof(IEnumerable<Code>), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
     public async Task<IEnumerable<Code>> GetOverview(CancellationToken cancellationToken)
